a3bc729f2a142f9e46457b891e3eb05c3b7e9fea
Kevin Ushey <kevinushey@gmail.com>
2015-12-03 15:03:27 -0800

    add some more sample commands

--- R/addins.R
+++ R/addins.R
@@ -1,3 +1,18 @@
 hello <- function() {
   print("Hello, World!")
 }
+
+random <- function() {
+  if (!requireNamespace("rstudioapi", quietly = TRUE))
+    return()
+
+  text <- as.character(runif(1))
+  rstudioapi::insertText(text)
+}
+
+insert_in <- function() {
+  if (!requireNamespace("rstudioapi", quietly = TRUE))
+    return()
+
+  rstudioapi::insertText(" %in% ")
+}
