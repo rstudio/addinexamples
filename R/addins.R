9e8182903b51745f2ced84c16ece5267f667f38c
Kevin Ushey <kevinushey@gmail.com>
2015-12-10 14:50:23 -0800

    import rstudioapi package

--- R/addins.R
+++ R/addins.R
@@ -3,16 +3,10 @@ hello <- function() {
 }
 
 random <- function() {
-  if (!requireNamespace("rstudioapi", quietly = TRUE))
-    return()
-
   text <- as.character(runif(1))
   rstudioapi::insertText(text)
 }
 
 insert_in <- function() {
-  if (!requireNamespace("rstudioapi", quietly = TRUE))
-    return()
-
   rstudioapi::insertText(" %in% ")
 }
